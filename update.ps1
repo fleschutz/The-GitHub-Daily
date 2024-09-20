188d3cfa7f2a9e6e7b3a293fb38372bc2712ccad
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-20 08:00:52 +0200

    Improved update.ps1

--- update.ps1
+++ update.ps1
@@ -72,4 +72,6 @@ try {
 	"(Generated automatically by PowerShell script update.ps1)"
 	exit 0 # success
 } catch {
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        exit 1
 }
