d31235af7db4ac54dfc40acde747fff1debf299b
Markus Fleschutz <markus.fleschutz@atcsim.de>
2024-09-20 07:41:47 +0200

    Updated README.md and update.ps1

--- update.ps1
+++ update.ps1
@@ -12,15 +12,14 @@
 #>
 
 function Repo([string]$name, [string]$URLpart) {
-	Start-Sleep -seconds 1
+	Start-Sleep -seconds 3
 	$releases = (Invoke-WebRequest -URI https://api.github.com/repos/$URLpart/releases?per_page=1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
 	foreach($release in $releases) {
-		"* [$name](https://github.com/$URLpart) $($release.name)"
-		return
-	}
-	$tags = (Invoke-WebRequest -URI https://api.github.com/repos/$URLpart/tags?per_page=1 -userAgent "curl" -useBasicParsing).Content | ConvertFrom-Json
-	foreach($tag in $tags) {
-		"* [$name](https://github.com/$URLpart) $($tag.name)"
+		if ($release.name -ne "") { $version = $release.name
+		} else {                    $version = $release.tag_name
+		}
+		if ($release.published_at -like "2024-09-20T*") { $version += "🆕" }
+		"* [$name](https://github.com/$URLpart) $version"
 		return
 	}
 }
@@ -28,40 +27,47 @@ function Repo([string]$name, [string]$URLpart) {
 try {
 	"The Daily GitHub - Latest Releases as of SEP 20"
 	"==============================================="
-	Repo "Bazel"              "bazelbuild/bazel"
-	Repo "cmake"              "Kitware/CMake"
 	Repo "curl"               "curl/curl"
-	Repo "Go"                 "golang/go"
-	Repo "Gradle"             "gradle/gradle"
 	Repo "Grafana"            "grafana/grafana"
-	Repo "Homebrew"           "Homebrew/brew"
 	Repo "Hugo"               "gohugoio/hugo"
-	Repo "Jenkins"            "jenkinsci/jenkins"
-	Repo "Julia"              "JuliaLang/julia"
-	Repo "Kotlin"             "JetBrains/kotlin"
 	Repo "Kubernetes"         "kubernetes/kubernetes"
 	Repo "Linux kernel"       "torvalds/linux"
-	Repo "LLVM"               "llvm/llvm-project"
 	Repo "Mastodon"           "mastodon/mastodon"
-	Repo "Ninja"              "ninja-build/ninja"
 	Repo "OBS Studio"         "obsproject/obs-studio"
 	Repo "OpenCV"             "opencv/opencv"
-	Repo "PowerShell"         "PowerShell/PowerShell"
 	Repo "PowerToys"          "microsoft/PowerToys"
 	Repo "Redis"              "redis/redis"
 	Repo "Ruby on Rails"      "rails/rails"
-	Repo "Rust"               "rust-lang/rust"
-	Repo "Scala"              "scala/scala"
 	Repo "Smartmontools"      "smartmontools/smartmontools"
-	Repo "Swift"              "swiftlang/swift"
-	Repo "TinyCC"             "TinyCC/tinycc"
-	Repo "TypeScript"         "microsoft/TypeScript"
 	Repo "Vim"                "vim/vim"
 	Repo "Visual Studio Code" "microsoft/vscode"
 	Repo "Windows Terminal"   "microsoft/terminal"
 	Repo "ZFS"                "openzfs/zfs"
 	""
-	"Generated automatically by script update.ps1."
+	"Programming Languages"
+	"---------------------"
+	""
+	Repo "Go"                 "golang/go"
+	Repo "Julia"              "JuliaLang/julia"
+	Repo "Kotlin"             "JetBrains/kotlin"
+	Repo "PowerShell"         "PowerShell/PowerShell"
+	Repo "Rust"               "rust-lang/rust"
+	Repo "Scala"              "scala/scala"
+	Repo "Swift"              "swiftlang/swift"
+	Repo "TypeScript"         "microsoft/TypeScript"
+	""
+	"Compiler &amp; Build Systems"
+	"----------------------------"
+	Repo "Bazel"              "bazelbuild/bazel"
+	Repo "CMake"              "Kitware/CMake"
+	Repo "Gradle"             "gradle/gradle"
+	Repo "Homebrew"           "Homebrew/brew"
+	Repo "Jenkins"            "jenkinsci/jenkins"
+	Repo "LLVM"               "llvm/llvm-project"
+	Repo "Ninja"              "ninja-build/ninja"
+	Repo "TinyCC"             "TinyCC/tinycc"
+	""
+	"(Generated automatically by PowerShell script update.ps1)"
 	exit 0 # success
 } catch {
 }
