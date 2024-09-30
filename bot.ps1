db28154f39b572a24c40f204284870ebcb938eee
Markus Fleschutz <markus.fleschutz@gmail.com>
2024-09-30 11:09:55 +0200

    Updated bot.ps1 and README.md

--- bot.ps1
+++ bot.ps1
@@ -49,7 +49,7 @@ try {
 	$ln += Repo "Redis"              "redis/redis"        ""
 	$ln += Repo "Smartmontools"      "smartmontools/smartmontools" "RELEASE_*"
 	$ln += Repo "ZFS"                "openzfs/zfs"        "zfs-*"
-	"**Today Sept. 28,** the latest releases in the **Featured** section are: $ln"
+	"**Today Sept. 30,** the latest releases in the **Featured** section are: $ln"
 	""
 	$ln = Repo "Atom"                "atom/atom"          "v*"
 	$ln += Repo "GNU Emacs"          "emacs-mirror/emacs" ""
