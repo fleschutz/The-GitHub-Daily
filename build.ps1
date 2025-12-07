1521afb75bfe0db4523bb51ad23c59e326189fe4
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-12-07 08:34:32 +0100

    Updated README.md

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@
 
 #requires -version 5.1
 
-param([string]$monthName = "December", [string]$monthPattern = "2025-12-*", [string]$newPattern = "2025-12-05*")
+param([string]$monthName = "December", [string]$monthPattern = "2025-12-*", [string]$newPattern = "2025-12-06*")
 
 function WriteREADME([string]$line) {
 	Write-Output $line >> README.md
