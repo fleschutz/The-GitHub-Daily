eb0e848d59504da50436d8bc2dbd0492c4ba56ab
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-01-10 11:33:15 +0100

    Updated README.md

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@
 
 #requires -version 5.1
 
-param([string]$monthName = "December", [string]$monthPattern = "2026-01-*", [string]$newPattern = "2026-01-08*")
+param([string]$monthName = "December", [string]$monthPattern = "2026-01-*", [string]$newPattern = "2026-01-09*")
 
 function WriteREADME([string]$line) {
 	Write-Output $line >> README.md
