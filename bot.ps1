2b94711f05fdeaaf13fe65fd4c9574750652e22e
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-05-02 09:08:56 +0200

    Updated README.md

--- bot.ps1
+++ bot.ps1
@@ -245,7 +245,7 @@ try {
 	$ln += Repo "Smartmontools"      "smartmontools/smartmontools" "RELEASE_*"
 	$ln += Repo "winapp"             "microsoft/winappCLI"   "v*"
 	$ln += Repo "zsh"                "zsh-users/zsh"         "zsh-*"
-	WriteLine "And last but not least **CLI** tools like $ln Data queried from $($global:numRepos) popular GitHub repos on $today by our friendly 🤖 [bot script](bot.ps1)."
+	WriteLine "And last but not least **CLI** tools like $ln - Page updated as of $today from $($global:numRepos) popular GitHub repos by our friendly 🤖 [bot script](bot.ps1)."
 	WriteLine ""
 
 	WriteLine "**Legend:** 🔥: *new release (24h)*, 🆕: *new release in $monthName*, 🔖: *new tag in $monthName*, 💤: *idle for >90 days*"
