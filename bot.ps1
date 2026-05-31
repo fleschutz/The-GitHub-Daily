e50cb63f10ed2fe3a4b2a592ae2a5211ff35a124
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-05-31 09:14:30 +0200

    Updated README.md

--- bot.ps1
+++ bot.ps1
@@ -14,7 +14,7 @@
 
 #requires -version 5.1
 
-param([string]$monthPattern = "2026-05-*", [string]$newPattern = "2026-05-28*")
+param([string]$monthPattern = "2026-05-*", [string]$newPattern = "2026-05-30*")
 
 function WriteLine([string]$line) {
 	Write-Output $line >> README.md
